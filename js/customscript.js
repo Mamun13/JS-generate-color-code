c1dd235272082a9cc46e434b39e5dd24790bd580
Al Mamun <mamuncse82@gmail.com>
2023-02-05 11:29:53 +0600

    update

--- js/customscript.js
+++ js/customscript.js
@@ -7,10 +7,11 @@ const generateHexColor = () => {
 	for (let i = 0; i < limit; i++) {
 		let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
 		randomHex = `#${randomHex.padStart(6, '0')}`;
-		console.log(randomHex);
-		const color = document.createElement('createcolor');
+		// console.log(randomHex);
+		const color = document.createElement('div');
+    console.log(color);
 		color.classList.add('color');
-		color.innerHTML = `<div class="col-lg-3 mt-3 mb-3 mn p-2">
+		color.innerHTML = `<div class="col-lg-3 mt-3 mb-2 mn p-2">
                         <div class="first p-1 shadow rounded ">
                           <div class="hex-color pt-3 ms-3 me-3 mt-3 rounded" style="background: ${randomHex}"></div>
                           <div class="hex-color-code text-center text-uppercase mt-2">${randomHex}</div>
