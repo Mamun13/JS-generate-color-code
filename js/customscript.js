b9f9bc4a55c4318458147757620b5d6755316580
Al Mamun <mamuncse82@gmail.com>
2023-02-05 10:13:09 +0600

    update

--- js/customscript.js
+++ js/customscript.js
@@ -1,23 +1,23 @@
 const refreshPage = document.querySelector('.refresh-btn');
-const show = document.querySelector('.row')
+const show = document.querySelector('.all-data');
 const limit = 40;
 
 const generateHexColor = () => {
-	for (let i = 0; i < limit ; i++) {
-    show.innerHTML = "";
+	show.innerHTML = '';
+	for (let i = 0; i < limit; i++) {
 		let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
-		randomHex = `#${randomHex.padStart(6, '0')}`; 
-		console.log(randomHex)
-    const color = document.createElement("p");
-    color.classList.add("color")
-    color.innerHTML = `<div class="col-lg-3 mt-3 mb-3 mn">
-                        <div class="first p-1 shadow rounded">
+		randomHex = `#${randomHex.padStart(6, '0')}`;
+		console.log(randomHex);
+		const color = document.createElement('createcolor');
+		color.classList.add('color');
+		color.innerHTML = `<div class="col-lg-3 mt-3 mb-3 mn p-2">
+                        <div class="first p-1 shadow rounded ">
                           <div class="hex-color pt-3 ms-3 me-3 mt-3 rounded" style="background: ${randomHex}"></div>
                           <div class="hex-color-code text-center text-uppercase mt-2">${randomHex}</div>
                         </div>
                       </div>`;
-    show.appendChild(color);   
-  }
+		show.appendChild(color);
+	}
 };
 generateHexColor();
 refreshPage.addEventListener('click', generateHexColor);
