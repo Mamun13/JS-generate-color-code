093d41dca3d452e67d15f8c29aa59dc8d915cbed
Al Mamun <mamuncse82@gmail.com>
2023-02-05 09:10:16 +0600

    generate color

--- js/customscript.js
+++ js/customscript.js
@@ -1,10 +1,23 @@
-const refreshPage = document.querySelector('.refresh-btn');
+const container = document.querySelector('.container');
+const refreshPage = document.querySelector('.mn');
+
+const limit = 40;
 
 const generateHexColor = () => {
-  // generate color code
-	let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
-	randomHex = `#${randomHex.padStart(6, '0')}`;
-	console.log(randomHex);
+	for (let i = 0; i >= limit; i++) {
+		// generate color code
+		let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+		randomHex = `#${randomHex.padStart(6, '0')}`;
+
+		const color = document.createElement('.first');
+		color.classList.add('color');
+		color.innerHTML = `<div class="first p-1 shadow rounded">
+                        <div class="hex-color pt-3 ms-3 me-3 mt-3 rounded"></div>
+                        <div class="hex-color-code text-center text-uppercase mt-2">#000000</div>
+                      </div>`;
+
+		refreshPage.appendChild(color);
+	}
 };
 
 refreshPage.addEventListener('click', generateHexColor);
